de2d0f78ab5109c6d399c481d61da04147d296ed
Arnaud293 <arnaudlt293@gmail.com>
2022-10-12 12:05:58 +0200

    add err/sucess messages

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -6,25 +6,42 @@ export const ContactUs = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
+    const formMess = document.querySelector('.form-message');
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('SERVICE_ID', 'TEMPLATE_ID', form.current, 'PUBLIC_KEY')
       .then((result) => {
           console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p class='sucess'> Message envoyé !</p>"
+
+          setTimeout(() => {
+            formMess.innerHTML = '';
+          }, 2500)
       }, (error) => {
           console.log(error.text);
+          formMess.innerHTML = "<p class='error'> Une erreur s'est produite. Merci de réessayer.</p>"
+          setTimeout(() => {
+            formMess.innerHTML = '';
+          }, 2500)
       });
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" required autoComplete='off'/>
-      <label>Email</label>
-      <input type="email" name="email" required autoComplete='off'/>
-      <label>Message</label>
-      <textarea name="message" required />
-      <input type="submit" value="Envoyer" />
-    </form>
+    <div className="form-container">
+        <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" required autoComplete='off'/>
+        <label>Email</label>
+        <input type="email" name="email" required autoComplete='off'/>
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Envoyer" />
+        </form>
+        <div className="form-message">
+
+        </div>
+    </div>
+
   );
 };
 
